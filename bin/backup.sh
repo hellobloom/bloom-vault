2b15e0e7708db198c95d3de59806612c7d0e376d
Eddie Hedges <eddiehedges@gmail.com>
2019-03-14 14:52:47 -0500

    Replicate backup clean logic from opengdpr service

--- bin/backup.sh
+++ bin/backup.sh
@@ -7,5 +7,15 @@ while true; do
     echo RUNNING BACKUP
     tar cvf /backup/vault_backup_$(date +%Y%m%d%H%M%S).tar /dbdata
     echo BACKUP COMPLETE
+
+    echo REMOVING OLD BACKUPS
+    # Deletes files in /backup that end in .tar and are older than BACKUP_DAYS_TO_KEEP
+    # Outputs to something like "find /backup -name *.tar -mtime +7 -type f -delete"
+    # The output of what files will be removed can be tested by removing the -delete
+    # Resources used:
+    # - use of find to delete: https://stackoverflow.com/a/13489511/1165441
+    # - default env var: https://stackoverflow.com/a/2013589/1165441
+    find /backup -name "*.tar" -mtime +${BACKUP_DAYS_TO_KEEP:-7} -type f -delete
+    echo OLD BACKUPS REMOVED
   fi
 done
