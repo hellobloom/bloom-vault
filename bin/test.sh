40bd60cf938da33b4be0a2e7f356964faa7e2b83
Eddie Hedges <eddiehedges@gmail.com>
2019-11-15 23:14:06 -0800

    update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -4,5 +4,6 @@ npm ci
 npm run build
 docker-compose -f docker-debug.yml up --build -d
 # sleep 30 give the docker-compose up plenty of time to run the web server for npm run test
-sleep 30; npm run test
+sleep 30
+npm run test
 docker-compose -f docker-debug.yml down
