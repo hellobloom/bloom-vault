3d8fedab7a0be6ca8c0e5383133add6c9bd0f8e8
Eddie Hedges <eddiehedges@gmail.com>
2019-11-15 23:17:14 -0800

    trying to make sure npm run test fails exit

--- bin/test.sh
+++ bin/test.sh
@@ -5,5 +5,5 @@ npm run build
 docker-compose -f docker-debug.yml up --build -d
 # sleep 30 give the docker-compose up plenty of time to run the web server for npm run test
 sleep 30
-npm run test
+npm run test || exit 1
 docker-compose -f docker-debug.yml down
