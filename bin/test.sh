3b1ea3fba24f434962aaf2a450735536fe1b800e
Eddie Hedges <eddiehedges@gmail.com>
2019-11-15 23:06:19 -0800

    add npm ci to test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Running bin/test.sh"
+npm ci
 npm run build
 docker-compose -f docker-debug.yml up --build -d
 # sleep 30 give the docker-compose up plenty of time to run the web server for npm run test
