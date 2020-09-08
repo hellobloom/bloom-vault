48a4c022c6b63c887328451d7b72539748614b4d
Eddie Hedges <eddiehedges@gmail.com>
2020-09-08 09:20:25 -0700

    Explicitly allow HTTP methods for CORS support

--- index.ts
+++ index.ts
@@ -33,6 +33,10 @@ if (env.trustProxy() === true) {
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, HEAD, OPTIONS'
+  )
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
