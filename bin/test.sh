54675cc0c068a00602ffb2ba27be4a877201b66b
Eddie Hedges <eddiehedges@gmail.com>
2020-10-27 21:51:20 -0700

    moar

--- bin/test.sh
+++ bin/test.sh
@@ -4,9 +4,9 @@ npm run build
 echo "npm run build succeeded"
 docker-compose -f docker-test.yml up --build -d
 echo "docker-compose up succeeded"
-# sleep 60 give the docker-compose up plenty of time to run the web server for npm run test
-sleep 60
-echo "slept 60"
+# sleep 90 give the docker-compose up plenty of time to run the web server for npm run test
+sleep 90
+echo "slept 90"
 bin_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 echo "bin_dir = $bin_dir"
 . $bin_dir/../.env.debug
@@ -15,12 +15,17 @@ echo "sourcing .env.debug succeeded"
 echo "postgres permissions config begin"
 su - postgres -c "createuser -s -i -d -r -l -w root"
 su - postgres -c "psql -c \"ALTER ROLE root WITH PASSWORD 'root';\""
+
+echo "before"
+docker ps
 docker-compose -f docker-test.yml ps
+echo "after"
+
 echo "pg pwd = $POSTGRES_PASSWORD, pg db = $POSTGRES_DATABASE"
 PGPASSWORD=$POSTGRES_PASSWORD dropdb --if-exists -h localhost -p 5434 -U jenkins -w $POSTGRES_DATABASE
 PGPASSWORD=$POSTGRES_PASSWORD createdb -h localhost -p 5434 -U jenkins $POSTGRES_DATABASE
 POSTGRES_USER=jenkins npm run test || exit 1
 echo "npm run test success"
 
-docker-compose -f docker-test.yml down
+docker-compose -f docker-test.yml down --volumes
 echo "docker-compose down"
