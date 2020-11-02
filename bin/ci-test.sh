25aa32b60f21055f932d799571e9fc865f353c0c
Eddie Hedges <eddiehedges@gmail.com>
2020-11-02 15:56:32 -0800

    ci

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -8,15 +8,9 @@ npm run build
 
 echo "Sourcing .env.test"
 bin_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-echo "bin_dir = $bin_dir"
 . $bin_dir/../.env.test
 
-echo "pg db: $POSTGRES_DATABASE"
-echo "pg user: $POSTGRES_USER"
-echo "pg pwd: $POSTGRES_PASSWORD"
-echo "node env: $NODE_ENV"
-
-echo "Postgres permissions config begin"
+echo "Postgres config for root"
 su - postgres -c "createuser -s -i -d -r -l -w root"
 su - postgres -c "psql -c \"ALTER ROLE root WITH PASSWORD 'DONTUSETHISPASSWORD';\""
 PGPASSWORD=$POSTGRES_PASSWORD dropdb --if-exists -h localhost -p 5432 -U root -w $POSTGRES_DATABASE
@@ -25,13 +19,15 @@ PGPASSWORD=$POSTGRES_PASSWORD createdb -h localhost -p 5432 -U root $POSTGRES_DA
 # echo "'cat'ing the contents from .env.test into .env for 'npm run start'"
 # cat $bin_dir/../.env.test > $bin_dir/../.env
 
-echo "Starting server for tests"
-npm run start & test_server_pid=$!
-echo "Started server with pid $test_server_pid"
+echo "Starting server for tests with .env.test environment vars"
+cat .env.test | xargs npm run start & test_server_pid=$!
+echo "Started server with pid '$test_server_pid'"
+
+echo "Sleeping for 15 seconds to give the server a chance to boot prior to tests"
 sleep 15
 
 echo "Running tests"
 npm run test || exit 1
 
-echo "Killing $test_server_pid"
+echo "Killing server with pid '$test_server_pid'"
 kill -9 $test_server_pid
